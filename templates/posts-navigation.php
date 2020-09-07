6a6daeeb9247b62b575aaa61faff305cafe306b0
ApinisMikelis <m.apinis92@gmail.com>
2020-09-07 21:49:49 +0300

    Posts navigation template and styling implemented.

---
+++ templates/posts-navigation.php
@@ -0,0 +1,8 @@
+<?php
+the_posts_navigation(
+    array(
+        'prev_text' => __( 'Iepriekšējie ieraksti', 'wp-bootstrap-starter' ),
+        'next_text' => __( 'Jaunāki ieraksti', 'wp-bootstrap-starter' ),
+        'screen_reader_text' => __( 'Posts navigation', 'wp-bootstrap-starter' )
+    )
+);
