fe1792cb96c358c7959a288d28a6d059795ee728
ApinisMikelis <m.apinis92@gmail.com>
2020-10-08 21:15:49 +0300

    Improved search page styling.

--- search.php
+++ search.php
@@ -16,18 +16,11 @@ get_header(); ?>
 		if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'wp-bootstrap-starter' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+				<h1 class="page-title"><?php printf( esc_html__( 'Jūsu meklējāt %s', 'wp-bootstrap-starter' ), '<span class="keyword">' . get_search_query() . '</span>' ); ?></h1>
+			</header>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			<?php while ( have_posts() ) : the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', 'search' );
 
 			endwhile;
@@ -40,9 +33,10 @@ get_header(); ?>
 
 		endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+		</main>
+
+	</section>
 
 <?php
-get_sidebar();
+
 get_footer();
