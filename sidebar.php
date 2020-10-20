fb6b4c6ec48dead6666c55c51b635e8109232013
ApinisMikelis <m.apinis92@gmail.com>
2020-10-20 17:15:27 +0300

    Made sidebar menu collapsable.

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,21 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area col-sm-12 col-lg-3" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+<aside id="secondary" class="widget-area col-sm-12 col-lg-3 " role="complementary">
+
+	<button class="sidebar-nav-toggler btn btn-light btn-block  d-lg-none"
+		type="button"
+		data-toggle="collapse"
+		data-target="#menu-wrap"
+		aria-controls=""
+		aria-expanded="false"
+		aria-label="Toggle navigation">
+		<span class="open-btn"><?php echo __( 'Atvērt izvēlni', 'wp-bootstrap-starter' ); ?> </span>
+		<span class="close-btn"><?php echo __( 'Aizvērt izvēlni', 'wp-bootstrap-starter' ); ?> </span>
+		<i class="fas fa-chevron-up"></i>
+	</button>
+
+	<div id="menu-wrap" class="collapse show">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>
+</aside>
